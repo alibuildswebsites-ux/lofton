6cda13dd9df156632349910db5945e324b814242
alibuildswebsites-ux <alibuildswebsites@gmail.com>
2026-02-07 21:46:47 +0000

    Auto-commit: 2026-02-07 21:46:47

--- components/TrustSection.tsx
+++ components/TrustSection.tsx
@@ -12,7 +12,16 @@ export const TrustSection = () => {
   useEffect(() => {
     const cards = gsap.utils.toArray('.gsap-trust-card');
     cards.forEach((card: any) => {
-      gsap.fromTo(card,
+      const tl = gsap.timeline({
+        scrollTrigger: {
+          trigger: card,
+          start: "top bottom",
+          end: "bottom top",
+          scrub: 1,
+        }
+      });
+
+      tl.fromTo(card,
         { 
           opacity: 0, 
           rotationX: -25, 
@@ -24,13 +33,20 @@ export const TrustSection = () => {
           rotationX: 0,
           y: 0,
           z: 0,
-          scrollTrigger: {
-            trigger: card,
-            start: "top bottom-=50",
-            end: "top center",
-            scrub: 1,
-          }
+          duration: 0.2,
+          ease: "none"
         }
+      )
+      .to(card, 
+        { 
+          opacity: 0, 
+          rotationX: 25, 
+          y: -60, 
+          z: -150,
+          duration: 0.2,
+          ease: "none" 
+        }, 
+        0.8
       );
     });
   }, []);
