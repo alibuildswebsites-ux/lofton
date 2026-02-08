71a9bec3988db3f69a4103aa9764ac4317d63bea
alibuildswebsites-ux <alibuildswebsites@gmail.com>
2026-02-08 19:58:38 +0000

    Auto-commit: 2026-02-08 19:58:38

--- components/PropertyListings.tsx
+++ components/PropertyListings.tsx
@@ -430,7 +430,7 @@ export const PropertyListings = () => {
             ) : currentProperties.length > 0 ? (
               <div className={`grid gap-6 ${viewMode === 'grid' ? 'grid-cols-1 md:grid-cols-2 xl:grid-cols-3' : 'grid-cols-1'}`} style={{ perspective: "1200px", transformStyle: "preserve-3d" }}>
                 {currentProperties.map(property => (
-                  <div key={property.id} className="gsap-list-card">
+                  <div key={property.id} className="gsap-list-card opacity-0">
                     <PropertyCard 
                       property={property}
                       viewMode={viewMode} 
