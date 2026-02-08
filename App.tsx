c9e2389dcd0a405c8e5899a53df006f3c04ce996
alibuildswebsites-ux <alibuildswebsites@gmail.com>
2026-02-08 08:11:43 +0000

    Auto-commit: 2026-02-08 08:11:43

--- App.tsx
+++ App.tsx
@@ -40,7 +40,6 @@ const SavedProperties = lazy(() => import('./components/dashboard/SavedPropertie
 // Admin Components
 const AdminDashboard = lazy(() => import('./components/admin/AdminDashboard').then(module => ({ default: module.AdminDashboard })));
 const LocationsPreview = lazy(() => import('./components/LocationsPreview'));
-const InteractiveMapDemo = lazy(() => import('./components/InteractiveMapDemo'));
 
 function App() {
   const location = useLocation();
@@ -79,7 +78,6 @@ function App() {
           <Route path="/forgot-password" element={<ForgotPasswordPage />} />
           <Route path="/admin/login" element={<AdminLoginPage />} />
           <Route path="/locations-preview" element={<LocationsPreview />} />
-          <Route path="/interactive-map" element={<InteractiveMapDemo />} />
           
           {/* Dashboard Routes - Protected */}
           <Route
